b594d248bdd3e2d7b14ae617e9fe56c1121d379c
luxeivan <luxeivan@gmail.com>
2025-04-18 10:26:45 +0300

    Изменил лого

--- src/components/headers/Header.jsx
+++ src/components/headers/Header.jsx
@@ -5,7 +5,8 @@ import Text from 'antd/es/typography/Text'
 import ButtonBack from './ButtonBack'
 import Container from '../Container'
 import useAuth from '../../store/authStore'
-import logo from '../../img/logo_mosoblenergo.svg'
+// import logo from '../../img/logo_mosoblenergo.svg'
+import logo from '../../img/MO_energo-logo-main.png'
 import styles from './header.module.css'
 
 export default function Header() {
@@ -17,7 +18,7 @@ export default function Header() {
     <Container>
       <Flex vertical>
         <Flex justify='space-between' align='center' style={{padding:20}} className={styles.topheader}>
-          <Image src={logo} preview={false}/>
+          <Image src={logo} preview={false} width={300}/>
           
           {user ?
             <Flex align='center' gap={20}>
